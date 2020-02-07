0ca0a202d72a7d732540107f51377f45fd10e52a
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-02-08 01:10:39 +0900

    add await-catch

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,31 @@ import RepoList from './RepoList'
 import github from '../api/github'
 
 class  App extends React.Component{
-  state = { starred_repos: [] }
+  state = { starred_repos: [], name: '', http_status: 200 }
 
   onSearchSubmit = async(name) => {
-    const response = await github.get(`/users/${name}/starred`);
-
-    this.setState({
-      starred_repos: response.data
-    })
+    let response = ''
+    this.setState({name: name})
+    try {
+      response = await github.get(`/users/${name}/starred`);
+      this.setState({
+        starred_repos: response.data
+      })
+    } catch (error) {
+      this.setState({
+        http_status: error.response.status
+      })
+    }
   }
   render(){
       return (
       <div>
         <SearchBar onSubmit={this.onSearchSubmit}/>
-        <RepoList repos={this.state.starred_repos}/>
+        <RepoList
+          repos={this.state.starred_repos}
+          name={this.state.name}
+          status_code={this.state.http_status}
+        />
       </div>
     )
   }
--- src/components/RepoList.js
+++ src/components/RepoList.js
@@ -2,10 +2,14 @@ import React from 'react';
 import RepoCard from './RepoCard'
 
 const RepoList = (props) => {
-  const repos = props.repos.map((repo)=>{
-    return <RepoCard key={repo.id} repo={repo}/>
-  })
-  return <div>{repos}</div>
+  if (props.status_code === 404 && props.name !== "") {
+    return <div>Error</div>
+  }else{
+    const repos = props.repos.map((repo)=>{
+      return <RepoCard key={repo.id} repo={repo}/>
+    })
+    return <div>{repos}</div>
+  }
 }
 
 export default RepoList

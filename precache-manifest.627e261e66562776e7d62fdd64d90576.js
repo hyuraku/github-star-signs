b97d71b8e88feb15ba1ad798edffe929cace0bec
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-04-05 20:45:12 +0900

    Updates

---
+++ precache-manifest.627e261e66562776e7d62fdd64d90576.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d338347cce279a8fe63563f3cb17d29",
+    "url": "/github-star-signs/index.html"
+  },
+  {
+    "revision": "2f10e980b20d81b55761",
+    "url": "/github-star-signs/static/css/main.5eabadf8.chunk.css"
+  },
+  {
+    "revision": "aebf9abb6c829197ccbc",
+    "url": "/github-star-signs/static/js/2.6360b404.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github-star-signs/static/js/2.6360b404.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f10e980b20d81b55761",
+    "url": "/github-star-signs/static/js/main.5ae43952.chunk.js"
+  },
+  {
+    "revision": "7ab4c33a297c03254bcc",
+    "url": "/github-star-signs/static/js/runtime-main.ab8a8f37.js"
+  }
+]);

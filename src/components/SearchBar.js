39c52712e08f9532aa40c6e5268f85dc2ff79700
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-04-27 22:26:32 +0900

    use useState in SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../css/SearchBar.css'
 
-class SearchBar extends React.Component {
-  state = { name: '' }
-
-  onFormSubmit = event => {
+const SearchBar = props => {
+  const [name, setName] = useState("")
+  const onFormSubmit = event => {
     event.preventDefault()
-
-    this.props.onSubmit(this.state.name)
+    props.onSubmit(name)
   }
-
-  render() {
-    return (
-      <div className="ui segment">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <h1>Enter GitHub username</h1>
-            <input
-              type="text"
-              autoFocus="autoFocus"
-              value={this.state.name}
-              onChange={e => this.setState({ name: e.target.value })}
-            />
-          </div>
-        </form>
+  return (<div className="ui segment">
+    <form onSubmit={onFormSubmit} className="ui form">
+      <div className="field">
+        <h1>Enter GitHub username</h1>
+        <input type="text" autoFocus="autoFocus" value={name} onChange={e => setName(e.target.value)}/>
       </div>
-    )
-  }
+    </form>
+  </div>)
 }
 
 export default SearchBar

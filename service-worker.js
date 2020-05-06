6da3d443e157515d101cc2fde63743bc670f8380
hyuraku <hyuraku@users.noreply.github.com>
2020-05-06 14:13:44 +0000

    Automated deployment: Wed May  6 14:13:44 UTC 2020 39e9476aae271b707ccf26e5f29f38edb181cd30

--- service-worker.js
+++ service-worker.js
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-star-signs/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });

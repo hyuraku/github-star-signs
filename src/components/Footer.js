f62a5ff917c1836c161bf9e26bcce7598c81bdae
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-07-17 23:02:36 +0900

    add fooer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../css/Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+      <footer>GitHub Star Sign is built with <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> . 
+            Source is on <a href="https://github.com/hyuraku/github-star-signs" target="_blank" rel="noopener noreferrer">GitHub</a>.</footer>
+    </div>
+  )
+}
+
+export default Footer

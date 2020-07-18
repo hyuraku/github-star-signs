4312bf2e63f5b4201dcc0dcb65270e412eeb146f
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-07-18 20:33:53 +0900

    add const

--- src/components/NoStarRepo.js
+++ src/components/NoStarRepo.js
@@ -2,9 +2,10 @@ import React from 'react'
 import '../css/NoStarRepo.css'
 
 const NoStarRepo = props => {
+  const {name} = props
   return (
     < div className = "ui container NoStarRepo" >
-      <h2> <span className="name">{props.name}</span> has no starred repository</h2>
+      <h2> <span className="name">{name}</span> has no starred repository</h2>
     </div>
   )
 }

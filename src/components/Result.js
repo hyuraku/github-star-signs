7f8f0c86e81b5e34fa2fad2e798cdbdd495a0338
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-11-14 16:05:25 +0900

    refactor

---
+++ src/components/Result.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { RepoList } from './RepoList'
+import { NoStarRepo } from './NoStarRepo'
+import { NameError } from './NameError'
+import { Loading } from './Loading'
+
+
+
+export const Result = (props) => {
+  const{ loading, name, http_status, starred_repos } = props
+  if (loading === true) return <Loading />
+  if (name !== '') {
+    if (http_status === 200) {
+      if (starred_repos.length === 0) {
+        return <NoStarRepo name={name} />
+      } else {
+        return <RepoList repos={starred_repos} />
+      }
+    } else {
+      return <NameError name={name} />
+    }
+  }
+  return ""
+}

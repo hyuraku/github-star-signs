bd6b96a6774051046348ec501f1f049dba3816b9
hyuraku <32809703+hyuraku@users.noreply.github.com>
2022-03-27 15:31:45 +0900

    yarn upgrade

--- snowpack.config.js
+++ snowpack.config.js
@@ -3,35 +3,37 @@ process.env.SNOWPACK_PUBLIC_URL
 module.exports = {
   mount: {
     public: {
-      url: '/'
+      url: '/',
     },
     src: {
-      url: '/dist'
+      url: '/dist',
     },
   },
   plugins: [
     '@snowpack/plugin-react-refresh',
-    "@snowpack/plugin-webpack",
+    '@snowpack/plugin-webpack',
     '@snowpack/plugin-typescript', // TS support
   ],
   /* for local SPA fallback routing support, more below */
-  routes: [{
-    "match": "routes",
-    "src": ".*",
-    "dest": "/index.html"
-  }, ],
+  routes: [
+    {
+      match: 'routes',
+      src: '.*',
+      dest: '/index.html',
+    },
+  ],
   devOptions: {
     port: 3000,
   },
   testOptions: {
-    files: ['src/**/*.test.*']
+    files: ['src/**/*.test.*'],
   },
   /* optional, if you want to use alias when importing */
   alias: {
-    "@app": "./src/",
+    '@app': './src/',
   },
   buildOptions: {
     metaUrlPath: 'snowpack',
     baseUrl: './',
   },
-};
+}

6fcb441357fb2e858f4af5ca8e241c1540f7982c
hyuraku <32809703+hyuraku@users.noreply.github.com>
2022-05-31 20:53:04 +0900

    repair

---
+++ snowpack.config-firefox.js
@@ -0,0 +1,39 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+process.env.SNOWPACK_PUBLIC_URL
+module.exports = {
+  mount: {
+    public: {
+      url: '/',
+    },
+    src: {
+      url: '/dist',
+    },
+  },
+  plugins: [
+    '@snowpack/plugin-react-refresh',
+    '@snowpack/plugin-webpack',
+    '@snowpack/plugin-typescript', // TS support
+  ],
+  /* for local SPA fallback routing support, more below */
+  routes: [
+    {
+      match: 'routes',
+      src: '.*',
+      dest: '/index.html',
+    },
+  ],
+  devOptions: {
+    port: 3001,
+  },
+  testOptions: {
+    files: ['src/**/*.test.*'],
+  },
+  /* optional, if you want to use alias when importing */
+  alias: {
+    '@app': './src/',
+  },
+  buildOptions: {
+    metaUrlPath: 'snowpack',
+    baseUrl: './',
+  },
+}

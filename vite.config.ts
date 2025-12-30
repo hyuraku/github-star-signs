1e680e8968deabdd0fde2fd5ee13642d4a504418
hyuraku <32809703+hyuraku@users.noreply.github.com>
2025-12-30 17:21:54 +0900

    Enable ESM support and clean up Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-const path = require('path')
-
 export default defineConfig({
   base: '/github-star-signs',
   plugins: [
